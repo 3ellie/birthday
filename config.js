98f7f84f4fa7b98ff06e147d743a9d29204a844e
3ellie <70436224+3ellie@users.noreply.github.com>
2020-08-29 13:31:50 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,13 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "Happy Birthday，Ellen",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+         "Happy Birthday，Ellen",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "LA la la", 
         "A wish for your wonderful birthday that",
         " brings a world of bright and", // 同上...
         "lovely things to remember each day", 
-        " through the year.", 
-    ],
+        " through the year.(*^▽^*)---Luxin", 
+          ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
      * "对应上面的文字, 要完全一样" : "图片地址, 可以把图片放在imgs文件夹中"
@@ -21,7 +22,7 @@ var config = {
     imgs: {
         "To ELLEN": "./imgs/xiaokeai.png",
         // "Today is your birthday. LALALA": "./imgs/birthday.jpg",
-    },
+            },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
         turn_on: "START",
@@ -30,7 +31,7 @@ var config = {
         balloons_flying: "missing sth.???",
         cake_fadein: "CAKE?",
         light_candle: "Candle？",
-        wish_message: "Happy Birthday Ellen---from Luxin",
+        wish_message: "Happy Birthday Ellen，wish you have A great time today！(p≧w≦q)",
         story: "A MESSAGE FOR YOU",
     }
 };
